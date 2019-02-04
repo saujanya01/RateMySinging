d94ea0850bfb95930b6a810dd32596fb1b0ad8fd
Saujanya Tiwari <saujanyatiwari455@gmail.com>
2019-02-04 23:26:58 +0530

    Toast Added

--- app/src/main/java/com/example/saujanya/ratemysinging/MainActivity.java
+++ app/src/main/java/com/example/saujanya/ratemysinging/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.saujanya.ratemysinging;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -40,7 +44,21 @@ public class MainActivity extends AppCompatActivity
                 HashMap<String ,String> desc=new HashMap<String, String>();
                 desc.put("name",str_name);
                 desc.put("age",str_age);
-                databaseReference.child(Integer.toString(count)).setValue(desc);
+                databaseReference.child(Integer.toString(count)).setValue(desc).addOnCompleteListener(new OnCompleteListener<Void>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this,"Data Uploaded",Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this,"Error in Uploading",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
                 count ++;
                 if (count >=10)
                 {
